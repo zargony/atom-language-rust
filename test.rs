8378f9a812ee7528670f1bcb236b44fdcac209a9
Andreas Neuhaus <zargony@zargony.com>
2015-03-02 17:05:54 +0100

    Fix indentation in test file

--- test.rs
+++ test.rs
@@ -84,16 +84,16 @@ text
 
 text
 pub enum MyEnum {
-  One,
-  Two
+    One,
+    Two
 }
 text
 
 text
 pub struct MyStruct<'foo> {
-  priv one: uint,
-  two: Option<'a, MyEnum>,
-  three: &'foo int,
+    priv one: uint,
+    two: Option<'a, MyEnum>,
+    three: &'foo int,
 }
 text
 
@@ -107,83 +107,83 @@ text
 
 text
 pub trait MyTrait {
-  text
-  fn create_something (param: &str, mut other_param: u32) -> Option<Self>;
-  text
-  fn do_whatever<T: Send+Share+Whatever, U: Freeze> (param: &T, other_param: u32) -> Option<U>;
-  text
-  fn do_all_the_work (&mut self, param: &str, mut other_param: u32) -> bool;
-  text
-  fn do_even_more<'a, T: Send+Whatever, U: Something<T>+Freeze> (&'a mut self, param: &T) -> &'a U;
-  text
+    text
+    fn create_something (param: &str, mut other_param: u32) -> Option<Self>;
+    text
+    fn do_whatever<T: Send+Share+Whatever, U: Freeze> (param: &T, other_param: u32) -> Option<U>;
+    text
+    fn do_all_the_work (&mut self, param: &str, mut other_param: u32) -> bool;
+    text
+    fn do_even_more<'a, T: Send+Whatever, U: Something<T>+Freeze> (&'a mut self, param: &T) -> &'a U;
+    text
 }
 text
 
 text
 impl<'foo> MyTrait for MyStruct<'foo> {
-  text
-  fn create_something (param: &str, mut other_param: u32) -> Option<Self> {
     text
-    return Some(cake);
-    text
-  }
-  text
-  fn do_whatever<T: Send+Share+Whatever, U: Freeze> (param: &T, other_param: u32) -> Option<U> {
-    assert!(1 != 2);
-    text
-    self.with_something(param, |arg1, arg2| {
-      text
-    }, other_param);
-  }
-  text
-  fn do_all_the_work (&mut self, param: &str, mut other_param: u32) -> bool {
-    announce!("There's no cake");
-    if !test_subject.under_control() {
-      text
-      let list: Vec<item> = some_iterator.map(|elem| elem.dosomething()).collect();
-      text
-      let boxed_list = box list;
-      text
-      self.announce_warning();
-      text
-      if test_subject.ignored_warnings > 3 {
+    fn create_something (param: &str, mut other_param: u32) -> Option<Self> {
         text
-        test_subject.incinerate();
-        text
-      }
-      text
-    }
-    text
-  }
-  text
-  fn do_even_more<'a, T: Send+Whatever, U: Something<T>+Freeze> (&'a mut self, param: &T) -> &'a U {
-    text
-    let foo: Option<'a uint> = Some(18);
-    text
-    if self.one < 1 {
-        text
-        self.complain(&foo);
+        return Some(cake);
         text
     }
-  }
-  text
+    text
+    fn do_whatever<T: Send+Share+Whatever, U: Freeze> (param: &T, other_param: u32) -> Option<U> {
+        assert!(1 != 2);
+        text
+        self.with_something(param, |arg1, arg2| {
+            text
+        }, other_param);
+    }
+    text
+    fn do_all_the_work (&mut self, param: &str, mut other_param: u32) -> bool {
+        announce!("There's no cake");
+        if !test_subject.under_control() {
+            text
+            let list: Vec<item> = some_iterator.map(|elem| elem.dosomething()).collect();
+            text
+            let boxed_list = box list;
+            text
+            self.announce_warning();
+            text
+            if test_subject.ignored_warnings > 3 {
+                text
+                test_subject.incinerate();
+                text
+            }
+            text
+        }
+        text
+    }
+    text
+    fn do_even_more<'a, T: Send+Whatever, U: Something<T>+Freeze> (&'a mut self, param: &T) -> &'a U {
+        text
+        let foo: Option<'a uint> = Some(18);
+        text
+        if self.one < 1 {
+            text
+            self.complain(&foo);
+            text
+        }
+    }
+    text
 }
 text
 
 text
 impl MyStruct<'foo> {
-  text
-  pub fn with_something<T: Send> (param: &T, f: |int, &str| -> T, other_param: u32) -> T {
     text
-    f(123, "hello")
+    pub fn with_something<T: Send> (param: &T, f: |int, &str| -> T, other_param: u32) -> T {
+        text
+        f(123, "hello")
+        text
+    }
     text
-  }
-  text
 }
 text
 
 'infinity: loop {
-  do_serious_stuff();
-  use_a_letter('Z');
-  break 'infinity;
+    do_serious_stuff();
+    use_a_letter('Z');
+    break 'infinity;
 }
